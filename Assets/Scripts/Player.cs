5cf75ce35f7061e3280f182bd57c2ed6777f439e
finalmegaman NA <finalmegaman@gmail.com>
2019-11-26 19:53:23 -0800

    188 Flip Character Sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Run();
+		FlipSprite();
 	}
 
 	void Run(){
@@ -24,5 +25,13 @@ public class Player : MonoBehaviour {
 
 		Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
 		myRigidBody.velocity = playerVelocity;
+	}
+
+	private void FlipSprite(){
+		bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+		if (playerHasHorizontalSpeed) {
+			transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+		}
 	}
 }
