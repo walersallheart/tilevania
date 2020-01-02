ad7c2a347ee8c3432633c90dba03ae85c125fe99
finalmegaman NA <finalmegaman@gmail.com>
2020-01-01 16:34:20 -0800

    201 Player Death.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour {
 	[SerializeField] float runSpeed = 5f;
 	[SerializeField] float jumpSpeed = 5f;
 	[SerializeField] float climbSpeed = 5f;
+	[SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
 
 	//State
 	bool isAlive = true;
@@ -31,10 +32,13 @@ public class Player : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (!isAlive){ return; }
+
 		Run();
 		ClimbLadder();
 		Jump();
 		FlipSprite();
+		Die();
 	}
 
 	private void Run(){
@@ -71,6 +75,14 @@ public class Player : MonoBehaviour {
 		if (CrossPlatformInputManager.GetButtonDown("Jump")){
 			Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
 			myRigidBody.velocity += jumpVelocityToAdd;
+		}
+	}
+
+	private void Die(){
+		if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy"))) {
+			myAnimator.SetTrigger("Dying");
+			GetComponent<Rigidbody2D>().velocity = deathKick;
+			isAlive = false;
 		}
 	}
 
