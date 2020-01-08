5d3fdd9a12721c5b13d751a609ac9ae30df326e9
finalmegaman NA <finalmegaman@gmail.com>
2020-01-07 19:20:29 -0800

    208 Persistent SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinPickup : MonoBehaviour {
+
+	[SerializeField] AudioClip coinPickupSFX;
 	private void OnTriggerEnter2D(Collider2D other) {
+		AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
 		Destroy(gameObject);
 	}
 }
