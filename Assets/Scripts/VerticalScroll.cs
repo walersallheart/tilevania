0dc997de37141a7e9f0283f5b998e726e4ea7ffa
finalmegaman NA <finalmegaman@gmail.com>
2020-01-07 20:06:22 -0800

    211 Adding Rising Water

---
+++ Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour {
+
+	[Tooltip("Game Units Per Second")]
+	[SerializeField] float scrollRate = 0.2f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0,scrollRate * Time.deltaTime,0);
+	}
+}
