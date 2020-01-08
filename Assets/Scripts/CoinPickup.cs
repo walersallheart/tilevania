1c27566155915a29edefe932bdc5782a1be5af7d
finalmegaman NA <finalmegaman@gmail.com>
2020-01-07 19:04:20 -0800

    207 Coin Pickups.

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D other) {
+		Destroy(gameObject);
+	}
+}
